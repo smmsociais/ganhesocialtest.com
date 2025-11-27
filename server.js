a6d671b9c78b4fe5cecd2b9db7d3283eb757680a
SMM <contato@ganhesocial.com>
2025-11-27 13:26:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,15 +11,20 @@ app.use(express.json());
 // Serve arquivos estáticos da raiz
 app.use(express.static(__dirname));
 
+// Rota API
+app.use("/api", async (req, res) => {
+  const { default: handler } = await import("./api/handler.js");
+  return handler(req, res);
+});
+
 // Rota da página inicial
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Rota API
-app.use("/api", async (req, res) => {
-  const { default: handler } = await import("./api/handler.js");
-  return handler(req, res);
+// Rota da página inicial
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "login.html"));
 });
 
 const PORT = process.env.PORT || 8080;
