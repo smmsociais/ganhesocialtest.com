198c771bbc75c62525e9c81f1e83c9faf0d8fa45
SMM <contato@ganhesocial.com>
2025-11-27 13:08:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import { logToFile } from "./logger.js";
 
 // Corrige caminhos dos arquivos
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -12,25 +11,6 @@ app.use(express.json());
 // Serve index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// Serve logs.html
-app.get("/logs", (req, res) => {
-  res.sendFile(path.join(__dirname, "logs.html"));
-});
-
-// Endpoint que retorna os logs
-app.get("/api/logs", (req, res) => {
-  const logPath = path.join(__dirname, "logs.txt");
-
-  // Garante que o arquivo exista
-  fs.access(logPath, fs.constants.F_OK, (err) => {
-    if (err) {
-      return res.json([]);
-    }
-    const logs = fs.readFileSync(logPath, "utf8").trim().split("\n");
-    res.json(logs.reverse());
-  });
 });
 
 // Qualquer outra rota passa para handler.js (se usar)
