75acbcc0e704759dfdc73d289ebbfaa5c8fbdfff
SMM <contato@ganhesocial.com>
2025-12-13 17:34:54 -0300

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -8,6 +8,7 @@ import { sendRecoveryEmail } from "./mailer.js";
 import crypto from "crypto";
 import { User, ActionHistory, DailyEarning, Pedido, DailyRanking } from "./schema.js";
 import express from "express";
+import jwt from "jsonwebtoken";
 
 // IMPORTAÇÃO DAS ROTAS INDEPENDENTES
 import buscarInstagram from "./buscar_acao_smm_instagram.js";
